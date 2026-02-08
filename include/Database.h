fae7eb32bd6687450411e725b4a78c74cbc1a795
Alexandr Drozd <alxdrzd.development@gmail.com>
2026-02-08 20:43:44 +0300

    Created Database class

---
+++ include/Database.h
@@ -0,0 +1,25 @@
+//
+// Created by alxdrzd on 2/8/26.
+//
+
+
+#pragma once
+
+#include <string>
+#include <memory>
+#include <pqxx/pqxx>
+
+#ifndef DATABASE_H
+#define DATABASE_H
+
+class Database {
+public:
+    explicit Database(const std::string& connection_string);
+    ~Database() = default;
+    pqxx::connection& get_connection() const;
+
+private:
+    std::unique_ptr<pqxx::connection> m_connection;
+};
+
+#endif //DATABASE_H
---
+++ src/Database.cpp
@@ -0,0 +1,22 @@
+#include "Database.h"
+#include <iostream>
+
+Database::Database(const std::string &connection_string) {
+    try {
+        m_connection = std::make_unique<pqxx::connection>(connection_string);
+
+
+        if (m_connection -> is_open()) {
+            std::cout << "[Database] Successfully connected to : " << m_connection->dbname() << std::endl;
+        } else {
+            std::cout << "[Database] Could NOT oped the DB" << std::endl;
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "[Database] Connection error : " << e.what() << std::endl;
+        throw;
+    }
+}
+
+pqxx::connection &Database::get_connection() const {
+    return *m_connection;
+}
